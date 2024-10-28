d98caea97f5743ce2ee794ae9df972db3d1fdbf9
JanKhoe <40544132+JanKhoe@users.noreply.github.com>
2024-10-28 19:15:01 -0300

    Add files via upload

--- src/app/components/Letter.tsx
+++ src/app/components/Letter.tsx
@@ -1,16 +1,17 @@
 interface LetterProps{
   children: React.ReactNode;
   letter: String;
+  className: string;
   // next: React.ReactElement;
   // onKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
 
 
-export default function Letter({children, letter}: LetterProps){
+export default function Letter({children, letter, className}: LetterProps){
 
   return (<>
-            <p>
+            <p className={className}>
               {letter}{children}
             </p>
           </>
