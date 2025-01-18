32a3d36c152fbfa7def96b7870963f03b199aa1e
JanKhoe <40544132+JanKhoe@users.noreply.github.com>
2025-01-18 13:21:09 -0400

    Add files via upload
    Attempt to debug why random paragraph selection isnt working on production build

--- src/app/api/paragraphs/route.ts
+++ src/app/api/paragraphs/route.ts
@@ -4,12 +4,16 @@ import { prisma } from "../primaClient";
 export async function GET() {
   const rows : number = await prisma.paragraphs.count();
   const randomIndex = Math.floor(Math.random() * rows);
+  console.log("~ JANSEN INPUT ~");
+  console.log(rows);
+  console.log(randomIndex);
   const RandomParagraph = await prisma.paragraphs.findMany(
     {
       skip: randomIndex,
       take: 1
     }
   )
+  console.log(await prisma.paragraphs.findMany());
 
   return NextResponse.json(RandomParagraph[0])
 }
