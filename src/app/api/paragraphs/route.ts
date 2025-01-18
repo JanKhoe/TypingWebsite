1f46d71c3a769120b5f3b9159200172b7a47d52a
JanKhoe <40544132+JanKhoe@users.noreply.github.com>
2025-01-18 13:29:29 -0400

    Add files via upload
    Attempt #2

--- src/app/api/paragraphs/route.ts
+++ src/app/api/paragraphs/route.ts
@@ -4,16 +4,12 @@ import { prisma } from "../primaClient";
 export async function GET() {
   const rows : number = await prisma.paragraphs.count();
   const randomIndex = Math.floor(Math.random() * rows);
-  console.log("~ JANSEN INPUT ~");
-  console.log(rows);
-  console.log(randomIndex);
   const RandomParagraph = await prisma.paragraphs.findMany(
     {
       skip: randomIndex,
       take: 1
     }
   )
-  console.log(await prisma.paragraphs.findMany());
 
-  return NextResponse.json(RandomParagraph[0])
+  return NextResponse.json(await prisma.paragraphs.findMany())
 }
